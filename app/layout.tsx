9d07fb890532feab599ce1cd3c5670b41a874f1f
Alan Dours <alandours93@gmail.com>
2023-11-10 22:34:56 -0300

    Move registry to lib

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
-import StyledComponentsRegistry from "@/utils/registry";
+import StyledComponentsRegistry from "@/lib/registry";
 
 import "./globals.css";
 
