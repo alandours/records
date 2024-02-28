61e96071b9cb2dde823d4d3eb794e75f4bf4eaf4
Alan Dours <alandours93@gmail.com>
2024-02-27 21:40:05 -0300

    Add key to loader

--- components/RecordsGrid/RecordsGrid.tsx
+++ components/RecordsGrid/RecordsGrid.tsx
@@ -12,11 +12,11 @@ type RecordsGridProps =
     };
 
 export const RecordsGrid = ({ releases, loader = false }: RecordsGridProps) => {
-  const items = Array(20).fill(
-    <div className="col-span-1 bg-gray-200">
+  const items = [...Array(20)].map((_, index) => (
+    <div className="col-span-1 bg-gray-200" key={index}>
       <div className="aspect-square w-[500px] max-w-full h-auto" />
     </div>
-  );
+  ));
 
   return (
     <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 gap-2 my-2">
