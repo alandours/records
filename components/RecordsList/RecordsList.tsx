17892a7ebc6c2ca96b604786c4d8a14d2252a24c
Alan Dours <alandours93@gmail.com>
2024-03-02 01:11:04 -0300

    Update styles: RecordList

--- components/RecordsList/RecordsList.tsx
+++ components/RecordsList/RecordsList.tsx
@@ -1,14 +1,16 @@
 import { ListRecord } from "@/components/ListRecord";
 import { Release } from "@/types";
 
+import { ListContainer } from "./styles";
+
 type RecordsListProps = {
   releases: Release[];
 };
 
 export const RecordsList = ({ releases }: RecordsListProps) => (
-  <div className="grid grid-cols-2 gap-4">
+  <ListContainer>
     {releases.map((release) => (
       <ListRecord release={release} key={release.instanceId} />
     ))}
-  </div>
+  </ListContainer>
 );
---
+++ components/RecordsList/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const ListContainer = styled.div`
+  display: grid;
+  gap: 1rem;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+`;
